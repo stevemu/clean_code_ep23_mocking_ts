3f5f0a230ba5da2929ad9c5e3c8e8b88f1f02587
Steve Mu <skycloud112@gmail.com>
2024-10-14 20:12:05 -0400

    simplify structure

--- packages/login/src/_test/LoginInteractorTest.spec.ts
+++ packages/login/src/_test/LoginInteractorTest.spec.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from '@jest/globals';
-import { LoginInteractorImpl } from '@repo/login/LoginInteractorImpl';
+import { LoginInteractorImpl } from '../usecases/LoginInteractorImpl';
 
 describe('LoginInteractor', () => {
   it('should login', () => {
